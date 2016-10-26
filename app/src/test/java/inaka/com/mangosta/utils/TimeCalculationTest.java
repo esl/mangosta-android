10d7dffc18a4b73658b96a9543dce97e24169010
Fernando Ramirez <f.e.ramirez94@gmail.com>
2016-10-26 17:43:14 -0300

    refactoring and improving unit tests

--- app/src/test/java/inaka/com/mangosta/utils/TimeCalculationTest.java
+++ app/src/test/java/inaka/com/mangosta/utils/TimeCalculationTest.java
@@ -4,44 +4,89 @@ package inaka.com.mangosta.utils;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Calendar;
 
 public class TimeCalculationTest {
 
+    private DateTime mReferenceDateTime;
+
+    @Before
+    public void initReferenceDateTime() {
+        mReferenceDateTime = getDateTime(2016, 10, 26, 15, 20, 34);
+    }
+
     @Test
-    public void checkMinutesDiffMax() throws Exception {
-        DateTime referenceDateTime = getDateTime(2016, 10, 26, 15, 20, 34);
+    public void checkMinutesDiffMaxWithSameDate() throws Exception {
+        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, mReferenceDateTime, 0));
+    }
 
-        // check same date
-        DateTime dateTime1 = getDateTime(2016, 10, 26, 15, 20, 34);
-        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(referenceDateTime, dateTime1, 0));
+    @Test
+    public void checkMinutesDiffMaxWithNegativeInput() throws Exception {
+        DateTime dateTime1 = getDateTime(2016, 10, 26, 15, 20, 55);
+        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime1, -1));
 
-        DateTime dateTime2 = getDateTime(2016, 10, 26, 15, 21, 34);
-        DateTime dateTime3 = getDateTime(2016, 10, 26, 15, 19, 34);
-        DateTime dateTime4 = getDateTime(2016, 10, 26, 15, 20, 20);
-        DateTime dateTime5 = getDateTime(3016, 10, 26, 15, 20, 34);
-        DateTime dateTime6 = getDateTime(2016, 10, 30, 15, 20, 34);
-        DateTime dateTime7 = getDateTime(2016, 10, 26, 15, 40, 34);
-        DateTime dateTime8 = getDateTime(2016, 10, 26, 9, 20, 34);
+        DateTime dateTime2 = getDateTime(2016, 10, 26, 15, 22, 55);
+        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime2, -24));
+    }
 
-        // true cases
-        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(referenceDateTime, dateTime2, 1));
-        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(referenceDateTime, dateTime2, 2));
-        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(referenceDateTime, dateTime3, 1));
-        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(referenceDateTime, dateTime4, 0));
-        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(referenceDateTime, dateTime7, 20));
-        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(referenceDateTime, dateTime7, 30));
-        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(referenceDateTime, dateTime8, 360));
+    @Test
+    public void checkMinutesDiffMaxWithSecondsDifference() {
+        DateTime dateTime1 = getDateTime(2016, 10, 26, 15, 20, 20);
+        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime1, 0));
 
-        // false cases
-        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(referenceDateTime, dateTime1, -1));
-        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(referenceDateTime, dateTime5, 5000));
-        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(referenceDateTime, dateTime6, 500));
-        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(referenceDateTime, dateTime7, 19));
-        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(referenceDateTime, dateTime7, 0));
-        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(referenceDateTime, dateTime8, 350));
+        DateTime dateTime2 = getDateTime(2016, 10, 26, 15, 21, 33);
+        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime2, 0));
+    }
+
+    @Test
+    public void checkMinutesDiffMaxWithMinutesDifference() {
+        DateTime dateTime1 = getDateTime(2016, 10, 26, 15, 21, 34);
+        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime1, 1));
+        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime1, 2));
+        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime1, 0));
+
+        DateTime dateTime2 = getDateTime(2016, 10, 26, 15, 19, 34);
+        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime2, 1));
+        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime2, 0));
+
+        DateTime dateTime3 = getDateTime(2016, 10, 26, 15, 25, 34);
+        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime3, 10));
+        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime3, 4));
+
+        DateTime dateTime4 = getDateTime(2016, 10, 26, 15, 40, 34);
+        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime4, 20));
+        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime4, 30));
+        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime4, 19));
+        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime4, 11));
+        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime4, 0));
+        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime4, -20));
+    }
+
+    @Test
+    public void checkMinutesDiffMaxWithDaysAndMonthsDifference() {
+        DateTime dateTime1 = getDateTime(2016, 10, 30, 15, 20, 34);
+        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime1, 500));
+
+        DateTime dateTime2 = getDateTime(2016, 10, 26, 9, 20, 34);
+        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime2, 360));
+        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime2, 350));
+
+        DateTime dateTime3 = getDateTime(2016, 11, 26, 9, 20, 34);
+        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime3, 45000));
+        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime3, 8000));
+    }
+
+    @Test
+    public void checkMinutesDiffMaxWithYearsDifference() {
+        DateTime dateTime1 = getDateTime(3016, 10, 26, 15, 20, 34);
+        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime1, 5000));
+
+        DateTime dateTime2 = getDateTime(2017, 11, 16, 15, 20, 34);
+        Assert.assertFalse(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime2, 5000));
+        Assert.assertTrue(TimeCalculation.isMinutesDiffMax(mReferenceDateTime, dateTime2, 10000000));
     }
 
     private static DateTime getDateTime(int year, int month, int day, int hour, int minute, int second) {
--- app/src/test/java/inaka/com/mangosta/utils/XMPPUtilsTest.java
+++ app/src/test/java/inaka/com/mangosta/utils/XMPPUtilsTest.java
@@ -8,26 +8,30 @@ import inaka.com.mangosta.xmpp.XMPPUtils;
 public class XMPPUtilsTest {
 
     @Test
-    public void checkJidToUserName() throws Exception {
+    public void checkJidToUserNameWithOurJidType() throws Exception {
         // @erlang-solutions.com JIDs
         Assert.assertEquals("ramabit", XMPPUtils.fromJIDToUserName("ramabit@erlang-solutions.com"));
         Assert.assertEquals("ramabit", XMPPUtils.fromJIDToUserName("ramabit@erlang-solutions.com/b4749661b09ddee8"));
         Assert.assertEquals("gardano", XMPPUtils.fromJIDToUserName("gardano@erlang-solutions.com"));
         Assert.assertEquals("gardano", XMPPUtils.fromJIDToUserName("gardano@erlang-solutions.com/b4749661b09ddee8"));
+    }
 
-        // other JIDs
-        Assert.assertEquals("flo@geekplace.eu", XMPPUtils.fromJIDToUserName("flo@geekplace.eu"));
+    @Test
+    public void checkJidToUserNameWithOtherJidTypes() throws Exception {
+        Assert.assertEquals("sarasa@sarasa.eu", XMPPUtils.fromJIDToUserName("sarasa@sarasa.eu"));
         Assert.assertEquals("hello@inaka.com", XMPPUtils.fromJIDToUserName("hello@inaka.com"));
     }
 
     @Test
-    public void checkUserNameToJid() throws Exception {
+    public void checkUserNameToJidWithOurJidType() throws Exception {
         // @erlang-solutions.com JIDs
         Assert.assertEquals("ramabit@erlang-solutions.com", XMPPUtils.fromUserNameToJID("ramabit"));
         Assert.assertEquals("gardano@erlang-solutions.com", XMPPUtils.fromUserNameToJID("gardano"));
+    }
 
-        // other JIDs
-        Assert.assertEquals("flo@geekplace.eu", XMPPUtils.fromUserNameToJID("flo@geekplace.eu"));
+    @Test
+    public void checkUserNameToJidWithOtherJidTypes() throws Exception {
+        Assert.assertEquals("sarasa@sarasa.eu", XMPPUtils.fromUserNameToJID("sarasa@sarasa.eu"));
         Assert.assertEquals("hello@inaka.com", XMPPUtils.fromUserNameToJID("hello@inaka.com"));
     }
 
