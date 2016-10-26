ebd048770be30f95b2fc291e83a30ee9576d59b6
Fernando Ramirez <f.e.ramirez94@gmail.com>
2016-10-26 10:46:51 -0300

    test user name to jid function

--- app/src/test/java/inaka/com/mangosta/utils/XMPPUtilsTest.java
+++ app/src/test/java/inaka/com/mangosta/utils/XMPPUtilsTest.java
@@ -8,7 +8,7 @@ import inaka.com.mangosta.xmpp.XMPPUtils;
 public class XMPPUtilsTest {
 
     @Test
-    public void checkUserNameToJid() throws Exception {
+    public void checkJidToUserName() throws Exception {
         // @erlang-solutions.com JIDs
         Assert.assertEquals("ramabit", XMPPUtils.fromJIDToUserName("ramabit@erlang-solutions.com"));
         Assert.assertEquals("ramabit", XMPPUtils.fromJIDToUserName("ramabit@erlang-solutions.com/b4749661b09ddee8"));
@@ -18,6 +18,17 @@ public class XMPPUtilsTest {
         // other JIDs
         Assert.assertEquals("flo@geekplace.eu", XMPPUtils.fromJIDToUserName("flo@geekplace.eu"));
         Assert.assertEquals("hello@inaka.com", XMPPUtils.fromJIDToUserName("hello@inaka.com"));
+    }
+
+    @Test
+    public void checkUserNameToJid() throws Exception {
+        // @erlang-solutions.com JIDs
+        Assert.assertEquals("ramabit@erlang-solutions.com", XMPPUtils.fromUserNameToJID("ramabit"));
+        Assert.assertEquals("gardano@erlang-solutions.com", XMPPUtils.fromUserNameToJID("gardano"));
+
+        // other JIDs
+        Assert.assertEquals("flo@geekplace.eu", XMPPUtils.fromUserNameToJID("flo@geekplace.eu"));
+        Assert.assertEquals("hello@inaka.com", XMPPUtils.fromUserNameToJID("hello@inaka.com"));
     }
 
 }
