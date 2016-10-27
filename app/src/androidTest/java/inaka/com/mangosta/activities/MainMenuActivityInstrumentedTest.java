dcc963bc3497cd9eee1f41662640e13b43c5349c
Fernando Ramirez <f.e.ramirez94@gmail.com>
2016-10-27 18:20:35 -0300

    add tests to check muc and muclight loads in chat lists

--- app/src/androidTest/java/inaka/com/mangosta/activities/MainMenuActivityInstrumentedTest.java
+++ app/src/androidTest/java/inaka/com/mangosta/activities/MainMenuActivityInstrumentedTest.java
@@ -1,6 +1,9 @@
 package inaka.com.mangosta.activities;
 
+import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -10,6 +13,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import inaka.com.mangosta.R;
 import inaka.com.mangosta.adapters.ViewPagerMainMenuAdapter;
 import inaka.com.mangosta.context.BaseInstrumentedTest;
 import inaka.com.mangosta.fragments.ChatsListFragment;
@@ -32,10 +36,52 @@ public class MainMenuActivityInstrumentedTest extends BaseInstrumentedTest {
         ChatsListFragment chatsListFragment = getChatsListFragment(ChatsListFragment.ONE_TO_ONE_CHATS_POSITION);
 
         // Check if it loads the correct amount of chats
-        int chatsCount = chatsListFragment.getChatListAdapter().getItemCount();
-        Assert.assertEquals(chatsCount, RealmManager.get1to1Chats().size());
+        Assert.assertEquals(getChatsCount(chatsListFragment), RealmManager.get1to1Chats().size());
 
         stopTiming(resource);
+    }
+
+    @Test
+    public void initializeMUCLightList() throws Exception {
+        Assume.assumeTrue(isUserLoggedIn());
+
+        // move to the 2nd tab
+        Espresso.onView(ViewMatchers.withId(R.id.viewpagerMainMenu))
+                .perform(ViewActions.swipeLeft());
+
+        IdlingResource resource = startTiming(10000);
+
+        // Obtain the one to one chats fragment
+        ChatsListFragment chatsListFragment = getChatsListFragment(ChatsListFragment.MUC_LIGHT_CHATS_POSITION);
+
+        // Check if it loads the correct amount of chats
+        Assert.assertEquals(getChatsCount(chatsListFragment), RealmManager.getMUCLights().size());
+
+        stopTiming(resource);
+    }
+
+    @Test
+    public void initializeMUCList() throws Exception {
+        Assume.assumeTrue(isUserLoggedIn());
+
+        // move to the 2nd tab
+        Espresso.onView(ViewMatchers.withId(R.id.viewpagerMainMenu))
+                .perform(ViewActions.swipeLeft())
+                .perform(ViewActions.swipeLeft());
+
+        IdlingResource resource = startTiming(10000);
+
+        // Obtain the one to one chats fragment
+        ChatsListFragment chatsListFragment = getChatsListFragment(ChatsListFragment.MUC_CHATS_POSITION);
+
+        // Check if it loads the correct amount of chats
+        Assert.assertEquals(getChatsCount(chatsListFragment), RealmManager.getMUCs().size());
+
+        stopTiming(resource);
+    }
+
+    private int getChatsCount(ChatsListFragment chatsListFragment) {
+        return chatsListFragment.getChatListAdapter().getItemCount();
     }
 
     private ChatsListFragment getChatsListFragment(int index) {
